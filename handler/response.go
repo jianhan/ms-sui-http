202df73ce208777bd9cb128dfb7cd118fc4127b0
Jian Han <jianhan@mail.com>
2018-07-06 19:37:55 +1000

    [DEV] Added JWT middleware

---
+++ handler/response.go
@@ -0,0 +1,18 @@
+package handler
+
+import (
+"net/http"
+"encoding/json"
+)
+
+func ResponseJSON(message string, w http.ResponseWriter, statusCode int) {
+	response := Response{message}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(jsonResponse)
+}
